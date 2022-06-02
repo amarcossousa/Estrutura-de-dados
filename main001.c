bb3ea58727d5de4b452542e9d735111a75a9b719
marcos-linux <antoniomarcus13@gmail.com>
2022-06-02 09:04:21 -0300

    pt01 - Introdução as estruturas de dados

---
+++ main001.c
@@ -0,0 +1,19 @@
+// INTRODUÇÃO
+// Tipos de dados int e float 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x = 10; //Declara como sendo int
+    int y = 20;
+    float z = x + y; //converte-os em float
+    printf(" A soma de a e b = %f\n", z); 
+}
+
+// Exemplo de implementação a partir do tipo de dados 
+
+
+
+// Tipo de dados abstratos TDA
+
